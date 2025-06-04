dfd64e7b9f6804ce0b94229cb893bf570cb38956
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2025-06-04 15:48:59 +0200

    Add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,18 @@
 </script>
 
 <template>
-  <header>RainbowTail</header>
-  <main>
-    Dummy Content
-  </main>
+  <div class="flex justify-center">
+    <div class="w-6xl">
+      <header class="text-4xl font-bold text-center py-8">
+        RainbowTail
+      </header>
+      <main class="">
+        Dummy content
+      </main>
+    </div>
+  </div>
 </template>
+
+<style>
+@import "tailwindcss";
+</style>
